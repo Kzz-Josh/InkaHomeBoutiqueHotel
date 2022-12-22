d0ff5b15db310ef39f82af4a23f074830d144f06
Josh <201913147g@utea.edu.pe>
2022-12-22 11:57:02 -0500

    upload fisrt

---
+++ ProyectoHotel/Logica/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoHotel.Logica
+{
+    public class Conexion
+    {
+        //public static string CN = "Data Source=.;Initial Catalog=DB_HOTEL;Integrated Security=True";
+
+        public static string CN = "Data Source=DBINKAHOTEL.mssql.somee.com;Initial Catalog=DBINKAHOTEL;user id=Kzz_SQLLogin_1;pwd=jjegkjfiwl";
+    }
+}
